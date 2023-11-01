fed5549bcc6905859a237993d7e97206a7bfb895
okb6 <okb6@cornell.edu>
2023-11-01 14:04:24 -0400

    trying sim

---
+++ thorvald_nvidia/loki/loki_base/loki_base/Py_Base_State.py
@@ -0,0 +1,17 @@
+import rclpy
+
+
+class PyBaseState():
+    def __init__(self):
+        
+        PyBaseState.drive_mode = 0
+        PyBaseState.prop_speed = [1, 2, 3, 4]
+        PyBaseState.prop_pos = []
+        PyBaseState.steer_speed = []
+        PyBaseState.steer_pos = [1, 2, 3, 4]
+        PyBaseState.steer_max_speed = []
+        PyBaseState.channel = []
+
+
+
+
